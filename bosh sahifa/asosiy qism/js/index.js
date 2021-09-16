c99c7e5131f52dd497ef333c9a11a543608d4ad1
abdumanno <amannon617@gmail.com>
2021-09-16 15:07:51 +0500

    8-o'zgartirish

--- bosh sahifa/asosiy qism/js/index.js
+++ bosh sahifa/asosiy qism/js/index.js
@@ -1,3 +1,12 @@
-let menu = document.querySelector("#menu").addEventListener("click", () => {
-        
-});
+const maktab = () =>{
+     const burger = document.querySelector("#menu"),
+     nav = document.querySelector(".menu"),
+     navlinks = document.querySelectorAll(".menu li");
+     burger.addEventListener("click",()=>{
+          nav.classList.toggle("nav-active");
+     });
+     navlinks.forEach((link,i)=>{
+          link.style.animation = `navmenu 0.5s ease forwards ${
+               i/7+0.35}s`;
+     });
+}
